4a3cb00d1bfca4db247676f2e19a32fc8c97bf46
Frank Chen <chentao76@hotmail.com>
2021-06-18 15:14:29 -0700

    included additional tenant

--- PS/Test.ps1
+++ PS/Test.ps1
@@ -1,6 +1,9 @@
 #testing
-$AdminUrl = "https://m365x725618-admin.sharepoint.com",
+$AdminUrl = "https://m365x725618-admin.sharepoint.com"
 $CredStoreName = "SPO-M365x725618"
+
+$psCred = Get-PnPStoredCredential -Name $CredStoreName -Type PSCredential
+Connect-SPOService -Url $AdminUrl -Credential $psCred
 
 #Get current ps running path
 $scriptFullPath = $MyInvocation.MyCommand.Path
@@ -26,3 +29,17 @@ Remove-SPOSiteDesign -Identity $sd_Communication.Id
 #remove the site script
 Remove-SPOSiteScript -Identity $siteScriptObj.Id
 
+Get-SPOSiteDesign
+
+Get-SPOSiteScript
+
+Set-SPOSiteScript -Identity 9d54d3a8-dc32-4125-9681-e2a542109c35 -Content $siteScriptJson_Communication
+
+Grant-SPOSiteDesignRights -Identity 16b3137e-34aa-4816-aa9c-b3d3eeb7d38b `
+    -Principals "frank@m365x725618.onmicrosoft.com" `
+    -Rights View
+
+Get-SPOSiteDesignRights -Identity 16b3137e-34aa-4816-aa9c-b3d3eeb7d38b
+Revoke-SPOSiteDesignRights -Identity 16b3137e-34aa-4816-aa9c-b3d3eeb7d38b
+Remove-SPOSiteDesign -Identity 7a4c6f4c-8cca-4e15-9d9d-8d8973b184d9
+Remove-SPOSiteScript -Identity 9d54d3a8-dc32-4125-9681-e2a542109c35
